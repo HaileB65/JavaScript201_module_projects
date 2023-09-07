3dc7f758aee177cfdf65c37a8b9d42c1dee1a162
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-09-07 14:46:45 -0500

    Fixing site generator bugs.

--- src/d.js
+++ src/d.js
@@ -1,14 +1,8 @@
-//  This function
-//  creates a hello world header element with a class of main-header
-//  creates a container element
-//  adds the header as a child of the container
-//  adds the container with a header as a child of the root element.
-
-const d = document
+const d = document;
 
 function c(tag, content = null, cssClass = null) {
-    element = d.createElement(tag)
-    if (content != null) element.innerText = content
-    if (cssClass!= null) element.classList.add(cssClass)
-    return element
+    element = d.createElement(tag);
+    if (content != null) element.innerText = content;
+    if (cssClass!= null) element.classList.add(cssClass);
+    return element;
 }
